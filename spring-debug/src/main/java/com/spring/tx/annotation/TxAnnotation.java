0d68a640e0b66701c9634582532df41dc7f95ac7
peter <1042397946@qq.com>
2022-01-28 09:34:38 +0800

    准备事务相关测试代码

--- spring-debug/src/main/java/com/spring/tx/annotation/TxAnnotation.java
+++ spring-debug/src/main/java/com/spring/tx/annotation/TxAnnotation.java
@@ -1,5 +1,20 @@
-package com.spring.tx.annotation;/**
-    @author ycb
-    @since 2022/1/27-15:45
-*/public class TxAnnotation {
+package com.spring.tx.annotation;
+
+import com.spring.tx.annotation.config.TransactionConfig;
+import com.spring.tx.annotation.service.BookService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author ycb
+ * @since 2022/1/27-15:45
+ */
+public class TxAnnotation {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		applicationContext.register(TransactionConfig.class);
+		applicationContext.refresh();
+		BookService bean = applicationContext.getBean(BookService.class);
+		bean.checkout("zhangsan",1);
+	}
 }
